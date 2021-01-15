697642be271b3b753892cf52cc19499e2a804dbe
bjbredis <bbarnes@gmail.com>
2021-01-15 16:58:24 -0500

    initial

---
+++ generate.py
@@ -0,0 +1,133 @@
+from redis import Redis
+from redis.exceptions import ResponseError,ConnectionError
+from os import environ,urandom,fork
+from uuid import uuid1,uuid4
+import random
+from time import sleep
+from logging import debug, info
+from faker import Faker
+import json
+import itertools
+
+'''
+    REDIS_HOSTNAME optional (default localhost)
+    REDIS_PORT optional (default 6379)
+    PREFIX_NAME optional (default cases)
+    COUNT (default 100000)
+    
+
+    Example:
+    $ export REDIS_HOSTNAME=localhost; export REDIS_PORT=14000; 
+    export INDEX_NAME=contracts; export COUNT=10000; 
+    python3 contracts.py
+
+'''
+
+'''
+returns a coherent record for a financial crimes case record
+
+'''
+def create_record(fake, id):
+    record = {}
+
+
+    statuses = ["new","investigating","resolved","on-hold","archived"]
+    priorities = ["low","med","high"]
+    date_range = [1389576338,1601861138] # Jan 13 2014 to Oct 5 2020
+
+    record["id"] = 10320000000 + id
+    record["status"] = statuses[random.randrange(0,len(statuses)-1)]
+    record["investigator"] = random.randrange(501001,501501)
+    record["value"] = int(abs(random.gauss(10000,300000)))
+
+    record["files"] = str(uuid1()).replace("-","")
+    for _ in itertools.repeat(None,random.randrange(1,8)):
+        record["files"] = record["files"] + "," + str(uuid4()).replace("-","")
+    record["date_reported"] = random.randrange(date_range[0],date_range[1])
+    record["date_last_updated"] = record["date_reported"] + random.randrange(1728000,8640000) # + 20 days or 100 days
+    record["report_body"] = fake.text()
+    record["primary_acctno"] = fake.ean8(prefixes=("41","82","21","97"))
+    record["ssn"] = str(fake.ssn()).replace("-","")
+    record["phone"] = fake.phone_number()
+    record["ip"] = fake.ipv4()
+    # first, middle, last, address, country, ip
+    record["account"] = json.dumps(
+        {"Full Name": fake.name(), 
+        "Street": fake.street_address(), 
+        "Country": "US", "State": "NB", 
+        "PostCode": fake.postcode()}
+    )
+    record["priority"] = priorities[random.randrange(0,len(priorities)-1)]
+    record["related_tags"] = record["primary_acctno"]
+    for _ in itertools.repeat(None,random.randrange(4,19)):
+        record["related_tags"] = record["related_tags"] + "," + fake.ean8(prefixes=("41","82","21","97"))
+    
+    # print(record)
+    return record
+
+
+
+
+if __name__ == '__main__':
+
+    batch = 5
+    count = environ.get('COUNT',100000)
+    redis_hostname = environ.get('REDIS_HOSTNAME','localhost')
+    redis_port = environ.get('REDIS_PORT',6379)
+    namespace = environ.get('NAMESPACE', "case") + ":"
+
+    r = Redis(host=redis_hostname, port=redis_port, decode_responses=True)
+    
+    fake = Faker()
+
+    counter = 0
+    while counter <= int(count):        
+        with r.pipeline(transaction=False) as pipe:
+            pipe.multi()
+            for _ in itertools.repeat(None, batch):
+                record = create_record(fake,counter)
+                if counter == 0: 
+                    print("Sample record: {}".format(json.dumps(record, indent=4)))
+                pipe.hset("{}{}".format(namespace,record["id"]),mapping=record)
+                counter = counter + 1
+            pipe.execute()
+        if counter % 1000 == 0 :
+            print(".",end="",flush=True)
+    print("\nFinished writing {} records.".format(counter))
+
+    # get some related tags from existing records
+    seeds = r.scan(match="{}*".format(namespace),count=3)[1]
+    taglist = list()
+    for result in seeds:
+        tags = r.hget(result,"related_tags").split(",")
+        for tag in tags[0:len(tags)-4]:
+            taglist.append(tag)    
+    random.shuffle(taglist) # shuffle so it is less deterministic
+
+    # create the list of tags to be added to cases
+    to_be_added_tags = ""
+    for i in range(1,batch):
+        to_be_added_tags  = to_be_added_tags  + "," + taglist[i]
+        # print("{},".format(taglist[i]),flush=False,sep='',end='')
+    print("--- Tags to be added: {}".format(to_be_added_tags))
+
+    for _ in itertools.repeat(None, random.randrange(batch,batch*3)): 
+        # # find one target case
+        # sample = random.sample(r.scan(match="{}*".format(namespace),count=15)[1],1)
+        # print("******\n*** Main target case: {} ***\n******".format(sample))
+
+        targets = list()
+        for _ in itertools.repeat(None, random.randrange(1,batch)):
+            targets.append(namespace + str(create_record(fake, random.randrange(1,int(count)))["id"]))
+        print("Targets: {}".format(targets))
+
+
+        for target in targets:
+            related_tags = r.hget(target,"related_tags")
+            related_tags = related_tags + to_be_added_tags
+            #put record back
+            r.hset(target,"related_tags",related_tags)
+    print("\n")
+            
+        
+        
---
+++ readme.md
@@ -0,0 +1,32 @@
+
+
+$ export COUNT=100; python generate.py
+Sample record: {
+    "id": 10320000000,
+    "status": "resolved",
+    "investigator": 501342,
+    "value": 195689,
+    "files": "2d83a34a577711eb89288c8590c28961,341353e0f0a74d688624d783638985f9,12a08f524fd8437a931040491f2a6179,b31f2fd79dc042e19cd616b047fa6f92",
+    "date_reported": 1413400514,
+    "date_last_updated": 1420499650,
+    "report_body": "Idea film interest window where. Finish debate other red foreign boy fly what.\nPass fact one picture structure fly. Special still ten staff life program discuss.",
+    "primary_acctno": "82021658",
+    "ssn": "763019603",
+    "phone": "445.470.7194x283",
+    "ip": "184.207.165.185",
+    "case_body": "{\"Full Name\": \"Raymond Trujillo\", \"Street\": \"2942 Sarah Route Apt. 975\", \"Country\": \"US\", \"State\": \"NB\", \"PostCode\": \"67047\"}",
+    "priority": "low",
+    "related_tags": "82021658,41525715,82098698,82527044,41750483,82849870,97004257,21609718,21198687,97558262,21392368,97550044,41410295,97347125,21578915,82367961,21098512,82173470,97919438"
+}
+
+Finished writing 105 records.
+--- Tags to be added: ,21450112,41938201,97605959,21240256
+Targets: ['case:10320000022', 'case:10320000013', 'case:10320000043']
+Targets: ['case:10320000033']
+Targets: ['case:10320000034', 'case:10320000082', 'case:10320000069']
+Targets: ['case:10320000001', 'case:10320000073', 'case:10320000062', 'case:10320000088']
+Targets: ['case:10320000053', 'case:10320000075', 'case:10320000032']
+Targets: ['case:10320000056', 'case:10320000007', 'case:10320000075', 'case:10320000052']
+Targets: ['case:10320000095', 'case:10320000062']
+Targets: ['case:10320000070', 'case:10320000078']
+Targets: ['case:10320000014', 'case:10320000098', 'case:10320000094']
